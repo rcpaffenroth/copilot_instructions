bac477955fe9bf4509b9969434016e9a51b702cc
Randy Paffenroth <randy.paffenroth@gmail.com>
2025-09-30 09:13:35 -0400

    Fixed typo in copilot-instructions.md

--- copy.sh
+++ copy.sh
@@ -5,4 +5,4 @@ echo "        $1/.github/copilot-instructions.md"
 cd $1
 mkdir -p .github
 cd .github
-cp $HOME/projects/0_core/copilot_instructions/.github/copilot-instructions.md .
+cp $HOME/projects/0_core/copilot_instructions/.github/copilot-instructions.md
